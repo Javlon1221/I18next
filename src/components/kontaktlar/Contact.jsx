06c925d0d581d4b7ee76c2cc51d5e7a7dbf2624c
Javlon1221 <avazbekzalilov835@gmail.com>
2025-06-08 14:19:58 +0500

    Refactor Contact component to use translation for text and improve structure

--- src/components/kontaktlar/Contact.jsx
+++ src/components/kontaktlar/Contact.jsx
@@ -1,35 +1,36 @@
-import React from 'react'
-import './contact.css'
+import React from 'react';
+import './contact.css';
+import { useTranslation } from 'react-i18next';
 
 const Contact = () => {
-  return (
-    <>
-        <div className="container">
-            <div className="contact">
-                <div className='kon'>
-                    <h1>Контакты </h1>
-                    <ul className='contactUl'>
-                        <li>г.Ташкент, Чиланзар 16-й квартал, ул. Ботирма</li>
-                        <li>+998970020771</li>
-                        <li>+998971970771 (Пн.-Пт. с 09:00 до 17:00)</li>
-                    </ul>
-                    <div className='contact_img'></div>
-                </div>
-            
-            <div className='bizbilan'>
-                <h1>Оставьте заявку</h1>
-                <p>В честь открытия нашей школы дарим скидку -20%</p>
-                <form className='contactForm' action="">
-                    <input className='form1' type="text" placeholder='Имя'/>
-                    <input className='form1' type="text" placeholder='Фамилия'/>
-                    <input className='form2' type="text" placeholder='Номер'/>
-                    <button>Отправить</button>
-                </form>
-            </div>
-            </div>
-        </div>      
-    </>
-  )
-}
+  const { t } = useTranslation();
 
-export default Contact
+  return (
+    <div className="container">
+      <div className="contact">
+        <div className='kon'>
+          <h1>{t('Kontaktlar.contact')}</h1>
+          <ul className='contactUl'>
+            <li>{t('Kontaktlar.manzil')}</li>
+            <li>{t('Kontaktlar.raqam')}</li>
+            <li>{t('Kontaktlar.raqam2')}</li>
+          </ul>
+          <div className='contact_img'></div>
+        </div>
+
+        <div className='bizbilan'>
+          <h1>{t('Komentlar.qoldirish')}</h1>
+          <p>{t('Komentlar.text')}</p>
+          <form className='contactForm' action="">
+            <input className='form1' type="text" placeholder={t('Komentlar.ism')} />
+            <input className='form1' type="text" placeholder={t('Komentlar.familiya')} />
+            <input className='form2' type="text" placeholder={t('Komentlar.raqam')} />
+            <button>{t('Komentlar.button')}</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
